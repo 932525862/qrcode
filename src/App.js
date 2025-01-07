e1eb50d5f5a54c3e4a377e661aab2cbae5efdf98
muhammadyusuf <muhammadyusuf@kali.muhammadyusuf>
2025-01-08 00:08:05 +0500

    malumot

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 // App.js
 import React from "react";
 import "./App.css"; // Tailwind CSS included here
-import Sertf from "./img/sertf1.png"
+import Sertf from "./img/sertf1.png";
+
 const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white">
-      <div className="w-full max-w-[210mm] h-[297mm]">
+    <div className="min-h-screen flex items-center justify-center bg-white px-4">
+      <div className="w-full max-w-[210mm] max-h-[297mm] aspect-[210/297]">
         <img
           src={Sertf}
           alt="A4 Document"
-          className="w-full h-full object-cover"
+          className="w-full h-full object-contain md:object-cover"
         />
       </div>
     </div>
