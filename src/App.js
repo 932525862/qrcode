12a88d8903e87e8184629ff62e3a3cfb3a589f7c
muhammadyusuf <muhammadyusuf@kali.muhammadyusuf>
2025-01-08 23:26:33 +0500

    malumot

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Layout } from "antd";
 import "./App.css"; // CSS faylini import qilamiz
-import Sertf from "./img/sertf14.jpg";
+import Sertf from "./img/sertifi55.jpg";
 const { Content } = Layout;
 
 function App() {
